cde723e955fc4a66441793df4b6655af9c6b56bb
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2024-12-11 18:23:21 +0400

    add already written functions declarations

--- libft.h
+++ libft.h
@@ -1,10 +1,20 @@
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-void *ft_memset(void *s, int c, size_t n);
-void ft_bzero(void *s, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		memcmp(const void *s1, const void *s2, size_t n);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
