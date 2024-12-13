b8173f50ffa2468ecabe27b4ef77850f45347f9b
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2024-12-14 02:32:46 +0400

    add last functions to header

--- libft.h
+++ libft.h
@@ -16,5 +16,11 @@ int		ft_isalpha(int c);
 int		ft_isalnum(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
