8e7d8d05a688f68b50c599c95bc5f7f95b59d495
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2024-12-15 00:35:32 +0400

    add ft_atoi

--- libft.h
+++ libft.h
@@ -24,5 +24,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_atoi(const char *nptr);
 
 #endif
